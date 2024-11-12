23c64fd373ac64ffbf762d262edfe3c44e0dfbeb
MLGM2001 <canonmauricio63@gmail.com>
2024-11-11 23:56:59 -0400

    actualizaciondelrolvista

--- dist/frontend/server/main.server.mjs
+++ dist/frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-3XZMECWZ.mjs";import"./chunk-B7YEO55V.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-KOCCN2EY.mjs";import"./chunk-B7YEO55V.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
